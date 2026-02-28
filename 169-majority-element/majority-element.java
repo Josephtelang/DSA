b7c36584542696ae21ce1cb4fd400319ffb40483
Josephtelang <josephtelang303@gmail.com>
2026-03-01 01:46:11 +0530

    Time: 2 ms (70.55%) | Memory: 55.6 MB (62.62%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -3,6 +3,9 @@ class Solution {
         int appearance = nums.length/2;
         int candidate = nums[0];
         int count =0 ;
+        if(nums.length ==0 || nums == null){
+            return -1;
+        }
         for (int i =0 ; i<nums.length ; i++){
             if(nums[i] == candidate){
                 count ++;

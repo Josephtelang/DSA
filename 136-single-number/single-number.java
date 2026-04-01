d18a5290b0b0860fed0a3a0fa03eeb4a6a63314b
Josephtelang <josephtelang303@gmail.com>
2026-04-02 01:35:21 +0530

    Time: 1 ms (99.98%) | Memory: 47.1 MB (37.65%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result =0;
+        int n = nums.length;
+        for (int i =0 ; i<n ; i++){
+            result = nums[i]^result;
+        }
+        return result;
+ 
+        
+    }
+}

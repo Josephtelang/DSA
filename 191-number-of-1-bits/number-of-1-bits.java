5adf140f2dedcaba163ec69bd98f1fea0134b546
Josephtelang <josephtelang303@gmail.com>
2026-04-02 14:00:33 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (74.71%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.java
+++ 191-number-of-1-bits/number-of-1-bits.java
@@ -1,20 +1,20 @@
 class Solution {
     public int hammingWeight(int n) {
         int count = 0;
-        int bitmask = 1;
-        while(n!=0){
-            if((n & bitmask) !=0){
-                count++;
-            }
-            n = n >> 1;
-        }
-        return count;
-
+        // int bitmask = 1;
         // while(n!=0){
-        //     n = n & (n-1);
-        //     count++;
+        //     if((n & bitmask) !=0){
+        //         count++;
+        //     }
+        //     n = n >> 1;
         // }
         // return count;
+
+        while(n!=0){
+            n = n & (n-1);
+            count++;
+        }
+        return count;
         
     }
 }

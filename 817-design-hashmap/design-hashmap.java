1923509a73f81d1cd5c4101f74db762bfa0d5161
Josephtelang <josephtelang303@gmail.com>
2026-04-08 15:12:18 +0530

    Time: 32 ms (44.06%) | Memory: 57.5 MB (39.32%) - LeetSync

--- 817-design-hashmap/design-hashmap.java
+++ 817-design-hashmap/design-hashmap.java
@@ -2,23 +2,23 @@ class MyHashMap {
     int array[];
 
     public MyHashMap() {
-        this.array = new int[1000001];
+        array = new int[1000001];
         Arrays.fill(array,-1);
         
     }
     
     public void put(int key, int value) {
-        this.array[key] = value;
+        array[key] = value;
         
     }
     
     public int get(int key) {
-        return this.array[key];
+        return array[key];
         
     }
     
     public void remove(int key) {
-        this.array[key] = -1;
+        array[key] = -1;
         
     }
 }

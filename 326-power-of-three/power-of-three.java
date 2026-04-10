bd6a2a32d5bd846b11826aa9ef49e6dfd7fe03c6
Josephtelang <josephtelang303@gmail.com>
2026-04-11 02:31:06 +0530

    Time: 8 ms (91.93%) | Memory: 45.8 MB (87.12%) - LeetSync

---
+++ 326-power-of-three/power-of-three.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if(n==1){
+            return true;
+        }
+        if(n==0 || n%3!=0 ){
+            return false;
+        }
+
+
+        int reminder = n/3;
+        return isPowerOfThree(reminder);
+
+    }
+}

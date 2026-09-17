b5aa7e7d18b8650804f7bcca68a5d212e97b90e4
Josephtelang <josephtelang303@gmail.com>
2026-09-18 01:52:33 +0530

    Time: 33 ms (5.67%) | Memory: 78.7 MB (5.02%) - LeetSync

---
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public static boolean getPath(TreeNode root,TreeNode n, ArrayList<TreeNode> path){
+        if(root == null){
+            return false;
+        }
+        path.add(root);
+        if(root == n){
+            return true;
+        }
+
+        boolean foundLeft = getPath(root.left,n,path);
+        boolean foundRight = getPath(root.right,n,path);
+        if(foundLeft || foundRight){
+            return true;
+        }
+
+        path.remove(path.size()-1);
+        return false;
+    }
+
+    public TreeNode lca1(TreeNode root, TreeNode p , TreeNode q){
+        if(root==null){
+            return null;
+        }
+        ArrayList<TreeNode> path1 = new ArrayList<>();
+        ArrayList<TreeNode> path2 = new ArrayList<>();
+
+        boolean foundLeft = getPath(root,p,path1);
+        boolean foundRight = getPath(root,q,path2);
+
+        if(!foundLeft || !foundRight){
+            return null;
+        }
+
+
+        int i=0;
+        for(;i<path1.size() && i<path2.size() ; i++){
+            if(path1.get(i).val != path2.get(i).val){
+                break;
+            }
+        }
+
+        return path1.get(i-1);
+
+    }
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        return lca1(root,p,q);
+    }
+}
